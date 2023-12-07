8555284304f74dd95c22aeef107e74b3dcf3d72b
Kotaiba Aal <kotaiba.aal@gmail.com>
2023-12-07 18:59:59 +0100

    Initial Commit

---
+++ Uppgift2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uppgift2
+//
+//  Created by Kotaiba Aal on 2023-12-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uppgift2/Uppgift2App.swift
@@ -0,0 +1,17 @@
+//
+//  Uppgift2App.swift
+//  Uppgift2
+//
+//  Created by Kotaiba Aal on 2023-12-07.
+//
+
+import SwiftUI
+
+@main
+struct Uppgift2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
